dbfe13f85593ed9ee800ed27dbc4fde2986b6695
Ilya Miroshnikov <miroshnikov.iv@gmail.com>
2021-11-04 17:43:09 +0300

    payload -> message

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ RESOURCE_DIR = os.path.join(BASE_FOLDER,"resourses")
 @app.route('/')
 def hello_world():
     with open(os.path.join(RESOURCE_DIR,"response.json")) as f:
-        return "%s - %s" % (json.loads(f.read()).get("payload"),datetime.datetime.now())
+        return "%s - %s" % (json.loads(f.read()).get("message"),datetime.datetime.now())
 
 if __name__ == "__main__":
                             app.run(host="0.0.0.0",port="8080",debug="true")
