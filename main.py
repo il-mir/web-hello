df47e1fc08eb249c77fc68eaba2cfbb8749ebe2b
Ilya Miroshnikov <miroshnikov.iv@gmail.com>
2021-11-08 15:40:58 +0300

    отформатировал вывод даты

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ RESOURCE_DIR = os.path.join(BASE_FOLDER,"resourses")
 @app.route('/')
 def hello_world():
     with open(os.path.join(RESOURCE_DIR,"response.json")) as f:
-        return "%s - %s" % (json.loads(f.read()).get("message"),datetime.datetime.now())
+        return "%s - %s" % (json.loads(f.read()).get("message"),datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S"))
 
 if __name__ == "__main__":
                             app.run(host="0.0.0.0",port="8080",debug="true")
